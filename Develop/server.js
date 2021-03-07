ada7d6fb85206ffb0efe0992c9fc5f7cd438850a
xena <74474948+a-andres1@users.noreply.github.com>
2021-03-07 16:46:24 -0500

    set up initial routes

--- Develop/server.js
+++ Develop/server.js
@@ -1,7 +1,29 @@
 // code for express
 const express = require('express');
+const path = require('path');
 
 // calling express, setting port
 const app = express();
-const port = 3300;
+const PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// setting up routes
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../Develop/public/notes.html'));
+  });
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../Develop/public/index.html'));
+  });
+
+
+
+
+// Listener
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
+
 
