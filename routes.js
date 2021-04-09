84898091c5365bb4da479f4e11fbbbf2d06e9b17
alyssa andres <alyssaandres1@gmail.com>
2021-04-09 15:36:02 -0400

    restructuring

---
+++ routes.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const fs = require('fs');
+
+
+module.exports = (app) => {
+// setting up routes
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '../Develop/public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../Develop/public/index.html'));
+});
+
+
+//   setting up api routes
+
+
+
+}
+
+
+
